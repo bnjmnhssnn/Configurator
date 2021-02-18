b1d95aab3eb76e4dca5788f511558797a6703f2d
bnjmnhssnn <info@benjamin-hosseinian.de>
2021-02-18 18:42:15 +0000

    1. Testumgebung weitergebaut 2. Choices-Yaml: Top Node "Choices" eingefügt

--- classes/Tree.php
+++ classes/Tree.php
@@ -9,42 +9,11 @@ class Tree
 {
     public function __construct(Config $config, Session $session)
     {
-        $session->foo = 'bar';
+        $configurator_tree = $config->get('plugins.configurator.configurator_tree');
+        $configurator_choices = $config->get('plugins.configurator.configurator_choices');
+        $configurator_session_values = $session->configurator ?? [];
 
-        
-        $this->tree = $config->get('plugins.configurator.configurator_tree');
 
-        /*
-        if(!$this->validateTree($this->tree, $error)) {
-            throw new ConfiguratorException($error);
-        }
-        */
-        /*
-        $this->tree['steps'] = array_map(
-            function($step) {
-                if($step['type'] === 'radio') {
-                    $step['options_radio_prepared'] = array_reduce(
-                        $step['options'],
-                        function($carry, $option) {
-                            $carry[$option['id']] = $option['name'];
-                            return $carry;
-                        },
-                        [] 
-                    );     
-                }
-                return $step;
-            },
-            $this->tree['steps']
-        );
-        */
-
-        /*
-        echo '<pre>'
-        . print_r($this->tree, true)
-        . print_r($session->getAll(), true) .
-        '</pre>';
-        exit;
-        */
         
     }
 
--- tests/TreeTest.php
+++ tests/TreeTest.php
@@ -1,12 +1,39 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use Grav\Plugin\Configurator\Tree;
+use Symfony\Component\Yaml\Yaml;
 
 final class TreeTest extends TestCase
 {
     public function testInstanciation()
     {
+        $config_mock = $this->getMock('Grav\Common\Config\Config', ['get']);
+        $config_mock->method('get')->will(
+            $this->returnCallBack(
+                function($path) {
+                    switch($path) {
+                        case 'plugins.configurator.configurator_tree':
+                            return Yaml::parse(file_get_contents(__DIR__ . '/configurator_tree.mock.yaml'));
+                    }    
+                }
+            )    
+        );
+
+        $session_mock = $this->getMock('Grav\Common\Session', []);
+
         $this->expectNotToPerformAssertions();
-        $tree = new Tree();
+        $tree = new Tree(
+            $config_mock,
+            $session_mock
+        );
     }
+
+    protected function getMock(string $classname, array $methods)
+    {
+        $mock = $this->getMockBuilder($classname)
+            ->setMethods($methods)
+            ->getMock();
+        return $mock;
+    }
+
 }
